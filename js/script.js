544c0c437e463db39f3f47c53344c0905152922e
Irina210921 <pux32008201@gmail.com>
2024-02-10 12:41:31 +0200

    new header+section+ correctwd html corrected css +section and carusel and li updated js ++ +mobile menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-$('.owl-carousel').owlCarousel({
-    autoplay:true,
-    autoplayTimeout:2000,
-    responsive:{
-        0:{
-            items:1
-        },
-        1100:{
-            items:5
-        }
-    }
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
 });
+
